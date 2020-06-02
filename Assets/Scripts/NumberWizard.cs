c3819b99a0f42c6a0723377b2e01146fbb22b92f
danielmartin0126 <danielmartin0126@gmail.com>
2020-06-01 22:58:06 -0400

    Quit button
    Added quit button to start menu. Also tweaked guessing logic to avoid duplicate guesses

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class NumberWizard : MonoBehaviour
 {
-    [SerializeField] int max = 1000;
-    [SerializeField] int min = 1;
-    int guess = (1000 + 1) / 2;
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
     [SerializeField] Text guessText;
 
 
@@ -20,27 +20,24 @@ public class NumberWizard : MonoBehaviour
 
     void StartGame()
     {
-        max = max + 1;
-        guess = (max + min) / 2;
-        guessText.text = guess.ToString();
-
+        NextGuess();
     }
 
     public void OnPressHigher()
     {
-        min = guess;
+        min = guess + 1;
         NextGuess();
     }
 
     public void OnPressLower()
     {
-        max = guess;
+        max = guess - 1;
         NextGuess();
     }
 
     void NextGuess()
     {
-        guess = (max + min) / 2;
+        guess = Random.Range(min, max + 1);
         guessText.text = guess.ToString();
     }
 }
