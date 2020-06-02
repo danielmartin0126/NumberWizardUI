cf39d92665b619f7510d2d414013ffb4816077e7
danielmartin0126 <danielmartin0126@gmail.com>
2020-06-01 21:29:42 -0400

    Game logic
    Imported guessing logic and attached to buttons

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max = 1000;
+    [SerializeField] int min = 1;
+    int guess = (1000 + 1) / 2;
+    [SerializeField] Text guessText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = max + 1;
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+    }
+}
